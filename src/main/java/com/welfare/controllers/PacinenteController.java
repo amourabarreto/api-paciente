e2537d5949a82372d3376cc8b97e7aaedcca7ed2
Andre Barreto <amourabarreto@gmail.com>
2022-07-13 16:00:51 -0300

    Validate e JsonView

--- src/main/java/com/welfare/controllers/PacinenteController.java
+++ src/main/java/com/welfare/controllers/PacinenteController.java
@@ -1,20 +1,49 @@
 package com.welfare.controllers;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.welfare.dtos.PacienteDTO;
+import com.welfare.models.PacienteModel;
+import com.welfare.services.PacienteService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Log4j2
 @RestController
 @CrossOrigin(origins = "*",maxAge = 3600 )
 @RequestMapping("/pacientes")
 public class PacinenteController {
 
+    @Autowired
+    PacienteService pacienteService;
+
+    @PreAuthorize("hasAnyAuthority('ROLE_ADM-MACHINE-APP_CAD_PACIENTE')")
+    @PostMapping("/anonimo")
+    ResponseEntity<Object> savePacienteAnonimo(@RequestBody
+                                               @Validated(PacienteDTO.PacienteView.AnonimoPost.class)
+                                               @JsonView(PacienteDTO.PacienteView.AnonimoPost.class)
+                                               PacienteDTO pacienteDTO){
+        log.debug("POST saveCourse CourseDTO RECEIVED {} ", pacienteDTO.toString());
+        var pacienteModel = new PacienteModel();
+        BeanUtils.copyProperties(pacienteDTO,pacienteModel);
+        pacienteService.save(pacienteModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteModel);
+    }
+
     @PreAuthorize("hasAnyAuthority('ROLE_ADM-MACHINE-APP_CAD_PACIENTE')")
     @PostMapping
     ResponseEntity<Object> save(@RequestBody @Valid PacienteDTO pacienteDTO){
-        return null;
+        log.debug("POST saveCourse CourseDTO RECEIVED {} ", pacienteDTO.toString());
+        var pacienteModel = new PacienteModel();
+        BeanUtils.copyProperties(pacienteDTO,pacienteModel);
+        pacienteService.save(pacienteModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteModel);
     }
 }
--- src/main/java/com/welfare/dtos/PacienteDTO.java
+++ src/main/java/com/welfare/dtos/PacienteDTO.java
@@ -1,9 +1,61 @@
 package com.welfare.dtos;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.welfare.models.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+import java.util.UUID;
 
 @Getter
 @Setter
 public class PacienteDTO {
+    public interface PacienteView {
+        public static interface AnonimoPost {
+        }
+    }
+    private UUID id;
+
+
+    private String cns;
+    private String prontuario;
+    @NotBlank(groups = PacienteView.AnonimoPost.class)
+    @Size(min = 4, max = 50, groups = PacienteView.AnonimoPost.class)
+    @JsonView(PacienteView.AnonimoPost.class)
+    private String nome;
+    private String nomeSocial;
+    private String apelido;
+    private String cpf;
+    private String dataNascimento;
+    private String naturalidade;
+    private String nomePai;
+    private String nomeMae;
+    private String dddFixo;
+    private String telefoneFixo;
+    private String dddCelular;
+    private String telegoneCelular;
+    private String userName;
+    private Boolean vreBacteria;
+    private String observacao;
+
+    private Set<EnderecoModel> enderecos;
+
+    private Set<RgModel> rgs;
+
+    private UfModel ufNaturalidadeModel;
+
+    private CorPeleModel corPeleModel;
+
+
+    private SexoModel sexoModel;
+
+
+    private NacionalidadeModel nacionalidadeModel;
+
+    private PaisModel paisModel;
+
+
 }
